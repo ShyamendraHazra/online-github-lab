3c5f8a3e76a13c35a5ffae8ec4f59e9e335db6cf
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-11-23 15:45:11 +0000

    set up library compilation

---
+++ build/CMakeFiles/TERMINAL_USER_INTERFACE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TERMINAL_USER_INTERFACE.
+# This may be replaced when dependencies are built.
---
+++ source/main.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hey user! welcome to the terminal!" << std::endl; 
+    return 0;
+}
